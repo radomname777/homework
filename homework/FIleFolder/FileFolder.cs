6a768fc37e606250bedd82d6b8cd933b71cf0ffa
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2022-07-21 02:50:51 +0400

    Add project files.

---
+++ homework/FIleFolder/FileFolder.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework.FIleFolder
+{
+    public  class FileFolder
+    {
+        private string _FileName { get; set; }
+        private FileFolder() {}
+        public FileFolder(string fileName="Source.txt")
+        {
+            _FileName = fileName;
+        }
+        public string Read(bool bol = false)
+        {
+
+
+           using FileStream fs = new FileStream(_FileName, FileMode.Open, FileAccess.Read);
+           using StreamReader sr = new StreamReader(fs);
+           return sr.ReadToEnd();
+        }
+        public  string CompressString(string text)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(text);
+            MemoryStream ms = new MemoryStream();
+            using (GZipStream zip = new GZipStream(ms, CompressionMode.Compress, true))
+
+                zip.Write(buffer, 0, buffer.Length);
+            
+
+            ms.Position = 0;
+            MemoryStream outStream = new MemoryStream();
+
+            byte[] compressed = new byte[ms.Length];
+            ms.Read(compressed, 0, compressed.Length);
+
+            byte[] gzBuffer = new byte[compressed.Length + 4];
+            System.Buffer.BlockCopy(compressed, 0, gzBuffer, 4, compressed.Length);
+            System.Buffer.BlockCopy(BitConverter.GetBytes(buffer.Length), 0, gzBuffer, 0, 4);
+            return Convert.ToBase64String(gzBuffer);
+        }
+
+
+
+
+        public void DecompressString()
+        {
+            using (Stream fileStream = File.OpenRead(_FileName),
+                     zippedStream = new GZipStream(fileStream, CompressionMode.Decompress))
+            {
+                using (StreamReader reader = new StreamReader(zippedStream))
+                    Console.WriteLine(reader.ReadToEnd());
+
+            }
+        }
+        public void Write(string data, bool bol = false)
+        {
+            using FileStream fs = new FileStream(_FileName, FileMode.Create, FileAccess.Write);
+            //if (bol) fs = new FileStream(_FileName, FileMode.Open, FileAccess.Write);
+            //else fs = new FileStream(_FileName, FileMode.Append, FileAccess.Write);
+            using StreamWriter sw = new StreamWriter(fs); sw.Write(data);
+
+    
+        }
+    }
+}
---
+++ homework/Program.cs
@@ -0,0 +1,109 @@
+﻿using homework.FIleFolder;
+using System;
+using System.Text;
+
+namespace MyApp
+{
+    interface DataSource
+    {
+        public static FileFolder FF { get; set; }
+        void WriteData(string data);
+        string ReadData();
+    }
+    public class FileDataSource : DataSource
+    {
+        public static FileFolder FF { get; set; } = new();
+
+        public FileDataSource(){}
+
+
+        public  string ReadData()
+        {
+            return FF.Read();
+        }
+
+        public void WriteData(string data)
+        {
+           FF.Write(data);
+        }
+    }
+    class DataSourceDecorator : DataSource
+    {
+        protected DataSource Wrappee { get; set; }
+        public static FileFolder FF { get; set; } = new();
+
+        public DataSourceDecorator(DataSource? source)
+        {
+
+            Wrappee = source;
+        }
+
+
+        public virtual void WriteData(string data) => Wrappee.WriteData(data);
+
+        public virtual string ReadData() => Wrappee.ReadData();
+    }
+    class EncryptionDecarator : DataSourceDecorator
+    {
+        public EncryptionDecarator(DataSource? source) : base(source)    { }
+        private bool _isok { get; set; } = false;
+        private string bytestring { get; set; }
+        public override string ReadData()
+        {
+            string Data = FF.Read();
+            if (!_isok) return null;
+            char[] ch = bytestring.ToCharArray();
+            Console.WriteLine(Data);
+            var Data2 = Encoding.Default.GetBytes(Data);
+            for (int i = 0; i < Data2.Length; i++)
+                foreach (var item in ch) Data2[i] ^= (byte)item;
+            _isok = false;
+
+            return Data;
+        }
+
+        public override void WriteData(string? data)
+        {
+            if (_isok) return;
+            string Data = FF.Read();
+            if (Data == null) return;
+            bytestring = data;
+            char[] ch = data.ToCharArray();
+            var Data2 = Encoding.Default.GetBytes(Data);
+            for (int i = 0; i < Data2.Length; i++)
+                foreach (var item in ch)Data2[i]^=(byte)item;
+            FF.Write(Encoding.Default.GetString(Data2));
+            _isok = true;
+        }
+    }
+    class CompressionDecorator : DataSourceDecorator
+    {
+        public CompressionDecorator(DataSource? source) : base(source){  }
+        public override string  ReadData()
+        {
+
+            FF.DecompressString();
+            return FF.Read();
+
+        }
+
+        public override void WriteData(string data)
+        {
+           
+            FF.Write(FF.CompressString(FF.Read()));
+        
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataSource sad = new FileDataSource() ;
+            sad = new DataSourceDecorator(sad);
+            sad.WriteData("A");
+            //sad.ReadData();
+          Console.WriteLine(sad.ReadData());
+        }
+    }
+}
